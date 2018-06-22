8286044120b3660d0a758db1c9933d47448937b2
James Kingsley <jameslkingsley@outlook.com>
2018-06-22 22:46:28 +0100

    Front end changes

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="relative flex items-center justify-center w-full h-full">
+        <span class="text-5xl uppercase tracking-extrawide font-thin border-b border-brand text-center">ARMA<strong class="font-bold">BOT</strong></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                //
+            }
+        },
+
+        computed: {
+            //
+        },
+
+        methods: {
+            //
+        }
+    }
+</script>
